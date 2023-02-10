bbe776de9bf7962d019c8d9010014eb9367ebe08
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-10 15:59:49 -0300

    Add files via upload

---
+++ semana1exe3/Program.cs
@@ -0,0 +1,17 @@
+﻿float  nota1, nota2, nota3, media; 
+Console.WriteLine("Insira a nota 1"); 
+nota1 = float.Parse(Console.ReadLine()); 
+Console.WriteLine("Insira a nota 2"); 
+nota2 = float.Parse(Console.ReadLine()); 
+Console.WriteLine("Insira a nota 3"); 
+nota3 = float.Parse(Console.ReadLine()); 
+media = (nota1+nota2+nota3)/3; 
+if (media >=6) {
+  Console.WriteLine($"Aluno aprovado com média {media}"); 
+}
+if (media<6 && media>=5) {
+  Console.WriteLine($"Aluno em recuperação com média {media}"); 
+}
+else if (media<5) {
+  Console.WriteLine($"Aluno reprovado com média {media}"); 
+}
