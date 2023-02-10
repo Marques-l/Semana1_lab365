e8d2b1037c847728d95419e5001084c53a133865
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-10 16:03:12 -0300

    Add files via upload

---
+++ semana1exe5/Program.cs
@@ -0,0 +1,26 @@
+﻿int lado1, lado2, lado3; 
+Console.WriteLine("Insira o valor do lado 1"); 
+lado1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Insira o valor do lado 2"); 
+lado2 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Insira o valor do lado 3"); 
+lado3 = int.Parse(Console.ReadLine()); 
+float soma1 = lado1 +lado2; 
+float soma2 = lado1+lado3; 
+float soma3 = lado2+lado3; 
+//forma um triângulo?
+if (soma1>lado3 || soma2> lado2 || soma3>lado1) {
+  Console.WriteLine("É um triângulo");
+  if (lado1==lado2 && lado2==lado3){
+    Console.WriteLine("Triângulo equilátero"); 
+  }
+  else if(lado1==lado2 || lado2==lado3 || lado1==lado3 ) {
+    Console.WriteLine("Triângulo isóceles");
+  }
+  else if (lado1!=lado2 && lado2!=lado3 && lado1!=lado3) {
+    Console.WriteLine("Triângulo escaleno"); 
+  } 
+} 
+ else {
+  Console.WriteLine("Não é um triângulo"); 
+}
