bec88fbd2d3e7d626816e82a3c15280713825ee2
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-10 16:12:10 -0300

    Add files via upload

---
+++ semana1exe2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); 
+double area, largura, altura; 
+Console.WriteLine("Insira altura"); 
+altura = double.Parse(Console.ReadLine()); 
+Console.WriteLine("Insira largura"); 
+largura = double.Parse(Console.ReadLine()); 
+area = (altura*largura/2);  
+Console.WriteLine($"A área do triângulo é {area}");  
