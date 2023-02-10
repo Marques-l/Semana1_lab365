7eb9a217c566a186b6bfcf4542f172711ba3bb2c
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-10 15:39:52 -0300

    Add files via upload

---
+++ exercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+String nome, sobrenome, cor;
+int idade; 
+Console.WriteLine("Qual é o seu nome?"); 
+nome = Console.ReadLine(); 
+Console.WriteLine("Qual é o seu sobrenome?"); 
+sobrenome = Console.ReadLine(); 
+Console.WriteLine("Qual é a sua idade?"); 
+idade = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Qual é a sua cor favorita?"); 
+cor = Console.ReadLine(); 
+Console.WriteLine($"Olá, seu nome é {nome} {sobrenome}, você tem {idade} anos e sua cor favorita é {cor}.");
---
+++ exercicio1/Semana1_lab365/README.md
@@ -0,0 +1,2 @@
+# Semana1_lab365_M1S01
+Primeira semana de exercícios 
