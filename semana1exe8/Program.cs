ee3fa9dcbf4c2716e938aef7f4fc7e900c516b4a
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-10 16:07:51 -0300

    Add files via upload

---
+++ semana1exe8/Program.cs
@@ -0,0 +1,10 @@
+﻿int val1 = 0, val2 = 1, i , val3,  n; 
+ n = 20; 
+Console.WriteLine("Série Fibonacci:"); 
+Console.Write(val1 + " " +val2 +" "); 
+for(i=2; i<n; ++i) {
+  val3=val1+val2; 
+  Console.Write(val3+" "); 
+  val1 = val2; 
+  val2 = val3; 
+}
